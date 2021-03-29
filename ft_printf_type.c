dc63db37472df4f3feaabf69610fa23d9b22ea59
Joao Ceia <joao.p.ceia@gmail.com>
2021-03-29 14:06:21 +0100

    handling special case of null pointer

--- ft_printf_type.c
+++ ft_printf_type.c
@@ -5,6 +5,8 @@ char *ft_ptrtoa(void *ptr)
 	char *hex;
 	char *out;
 
+	if (!ptr)
+		return (ft_strdup("(nil)"));
 	hex = ft_lltoa_base((unsigned long)ptr, "0123456789abcdef");
 	out = ft_strjoin("0x", hex);
 	free(hex);
