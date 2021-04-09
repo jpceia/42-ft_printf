9469215e3f0058a9a3da84144ab747c8fd967595
Joao Ceia <joao.p.ceia@gmail.com>
2021-04-09 15:47:20 +0100

    changes in libft (added bonus funcs)

--- libft/ft_contains_bonus.c
+++ libft/ft_contains_bonus.c
@@ -1,22 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_contains_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/29 06:05:31 by jpceia            #+#    #+#             */
-/*   Updated: 2021/04/04 09:32:41 by jpceia           ###   ########.fr       */
+/*   Created: 2021/04/04 09:32:11 by jpceia            #+#    #+#             */
+/*   Updated: 2021/04/04 09:32:12 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-#define LIBFT_BONUS_H
-
-#include "libft.h"
-
-char	*ft_lltoa(long long nb);
-char	*ft_lltoa_base(long long nb, const char *base);
-int		ft_contains(char c, char *s);
-
-#endif
+int	ft_contains(char c, char const *charset)
+{
+	while (*charset)
+		if (c == *charset++)
+			return (1);
+	return (0);
+}
