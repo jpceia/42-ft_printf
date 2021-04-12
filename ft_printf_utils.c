0be08af521c59552c25aa5e92e6b57c4be6093b6
Joao pedro Ceia <jceia@c1r18s6.42lisboa.com>
2021-04-12 20:18:31 +0100

    freeing string in dynamic memmory to avoid leak

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 12:03:44 by jpceia            #+#    #+#             */
-/*   Updated: 2021/04/04 23:19:25 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/04/12 18:25:27 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ char	*adjust_format_width_space(char *s, int width, int left)
 		s2 = malloc(len + 1);
 		ft_memset(s2, ' ', len);
 		s2[len] = '\0';
+		free(s);
 		if (left)
 			s = ft_strjoin(s1, s2);
 		else
