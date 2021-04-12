68ba4cfa99ec1dfdeac383f4dc984cd5fac34873
Joao pedro Ceia <jceia@c1r18s6.42lisboa.com>
2021-04-12 20:17:49 +0100

    changed way to handle pointers

--- ft_printf_pointer.c
+++ ft_printf_pointer.c
@@ -6,19 +6,19 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 09:50:40 by jpceia            #+#    #+#             */
-/*   Updated: 2021/04/12 16:39:04 by jceia            ###   ########.fr       */
+/*   Updated: 2021/04/12 19:13:06 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_utils.h"
 
-char	*ft_ptrtoa(unsigned long nb)
+char	*ft_ptrtoa(void *ptr)
 {
 	char	*hex;
 	char	*pre;
 	char	*s;
 
-	hex = ft_lltoa_base(nb, "0123456789abcdef");
+	hex = ft_ulltoa_base((unsigned long long)ptr, "0123456789abcdef");
 	pre = ft_strdup("0x");
 	s = ft_strjoin(pre, hex);
 	free(pre);
@@ -28,15 +28,15 @@ char	*ft_ptrtoa(unsigned long nb)
 
 int	print_pointer(va_list *args, t_spec spec)
 {
-	int				n_chars;
-	unsigned long	nb;
-	char			*s;
+	int		n_chars;
+	void	*ptr;
+	char	*s;
 
-	nb = va_arg(*args, unsigned long);
-	if (!nb)
+	ptr = va_arg(*args, void *);
+	if (!ptr)
 		s = ft_strdup("0x0");
 	else
-		s = ft_ptrtoa(nb);
+		s = ft_ptrtoa(ptr);
 	s = adjust_format_width_space(s, spec.width, spec.minus);
 	ft_putstr_fd(s, STDOUT_FILENO);
 	n_chars = ft_strlen(s);
