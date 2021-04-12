aa7e1547798433527b650203259f40f69a4228ec
Joao pedro Ceia <jceia@c1r18s6.42lisboa.com>
2021-04-12 20:20:08 +0100

    added ulltoa functions to libft

--- libft/libft_bonus.h
+++ libft/libft_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 06:05:31 by jpceia            #+#    #+#             */
-/*   Updated: 2021/04/12 17:08:59 by jceia            ###   ########.fr       */
+/*   Updated: 2021/04/12 19:11:49 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 #include "libft.h"
 
 char	*ft_lltoa(long long nb);
+char	*ft_ulltoa(unsigned long long nb);
 char	*ft_lltoa_base(long long nb, const char *base);
+char	*ft_ulltoa_base(unsigned long long nb, const char *base);
 int		ft_contains(char c, char *s);
 char	*ft_strreverse(char *s);
 
